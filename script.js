7f1470bbe7ad137c6626adc4e0fa7e57e0009a43
Biner-h <Binerhabib@gmail.com>
2023-09-15 14:45:47 +0200

    update

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 let app = document.getElementById("app");
 
+let text = "";
+let counter = "";
+
 //View
 updateView();
 function updateView() {
     app.innerHTML = /*html*/ `
-        <input type="text" id="inputStreng">
-        <button onclick="telleVokaler()">Tell Vokaler</button>
+        <input class="input" type="text" onchange="text = this.value" value = '${text}'>
+        <button class="knapp" onclick="telleVokaler(text)">Tell Vokaler</button>
         <p>Resultat:</p>
-        <div id="resultat"></div>
+        <div class="resultat" id="resultat">${counter}</div>
     `;
 }
 
-function telle
+function telleVokaler(string) {
+    let vokaler = "aeiouyæøå";
+    let vokalTeller = 0;
+
+    for (let i = 0; i < string.length; i++) {
+        let bokstav = string[i];
+        if (vokaler.includes(bokstav)) {
+            vokalTeller++;
+        }
+    }
+    counter = vokalTeller;
+    updateView();
+}
